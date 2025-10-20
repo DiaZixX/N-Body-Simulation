c38bfd666b6cd039ab5185db4d2acffbf808f139
Jules <julesbodin04@gmail.com>
2025-10-20 10:40:54 +0200

    feat(geom) : Add Vec2 struct for 2D points

---
+++ src/geom/vec2.rs
@@ -0,0 +1,27 @@
+use std::fmt;
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct Vec2 {
+    pub x: f32,
+    pub y: f32,
+}
+
+impl Vec2 {
+    pub fn new(x: f32, y: f32) -> Self {
+        Self { x, y }
+    }
+
+    pub const fn zero() -> Self {
+        Self { x: 0.0, y: 0.0 }
+    }
+
+    pub fn distance(self, other: Vec2) -> f32 {
+        ((self.x - other.x).powi(2) + (self.y - other.y).powi(2)).sqrt()
+    }
+}
+
+impl fmt::Display for Vec2 {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
+    }
+}
